6db87c064df044d7fce9fae28e7be5ba4e8e8e6d
JonTheMathMan <suspect@example.com>
2017-05-31 11:03:57 -0700

    switching to Objects
    shouldn't be a class.

--- mancala/program/classes/HousePocket.js
+++ mancala/program/classes/HousePocket.js
@@ -1,15 +1,3 @@
-class HousePocket
-	{
-		constructor(leftX=0,topY=0)
-		{
-			var houseGraphic = createImg(leftX,topY,houseProperties);
-			this.gemAmount=0;
-			houseGraphic.creator = this;
-			houseGraphic.addEventListener("click",this.moveGems);
-		}
-		
-		moveGems(e)
-		{
-			
-		}
-	}
+var houseGraphic = {
+	"image":createImg(45,32,houseProperties);
+}
