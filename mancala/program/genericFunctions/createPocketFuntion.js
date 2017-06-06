4fd6d9d8dd4f0a3de6025ad418d36e500322b280
JonTheMathMan <suspect@example.com>
2017-06-06 12:25:50 -0700

    made createPocket() more readable
    made the objects used in createPocket() easier to read.

--- mancala/program/genericFunctions/createPocketFuntion.js
+++ mancala/program/genericFunctions/createPocketFuntion.js
@@ -1,11 +1,24 @@
 function createPocket(passedInProperties={})
 {
 	//make the pocket img
-	var newPocket = addElementWithPropsToBoard({elementType:"img",src:passedInProperties.source,left:passedInProperties.left,top:passedInProperties.top,gemsHeld:passedInProperties.gems,pocketType:passedInProperties.pocketType});
+	var pocketProperties = {
+		elementType:"img",
+		src:passedInProperties.source,
+		left:passedInProperties.left,
+		top:passedInProperties.top,
+		gemsHeld:passedInProperties.gems,
+		pocketType:passedInProperties.pocketType
+	};
+	var newPocket = addElementWithPropsToBoard(pocketProperties);
 	newPocket.addEventListener("click",function(e){gemMove(e.target);});
 	
 	//make the gems textarea
-	var gemsDisplay = addElementWithPropsToBoard({left:passedInProperties.left,top:passedInProperties.top-20,value:passedInProperties.gems});
+	var gemsDisplayProperties = {
+		left:passedInProperties.left,
+		top:passedInProperties.top-20,
+		value:passedInProperties.gems
+	};
+	var gemsDisplay = addElementWithPropsToBoard(gemsDisplayProperties);
 	gemsDisplay.style.width = 50;
 	gemsDisplay.style.height = 20;
 	gemsDisplay.style.resize = "none";
